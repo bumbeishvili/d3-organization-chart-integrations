1f0fe6d8b50ff7143c943983c7efb98e7081b9b4
bumbeishvili <davit.bumbeishvili@gmail.com>
2019-11-20 17:14:54 +0400

    Vue.js Demo

---
+++ vue/org-chart-demo/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <form @submit.prevent="getData">
+      <div>
+        <button>expand node with id O-111</button>
+        <button>collapse node with id O-111</button>
+        <button>add node as root's child</button>
+        <button>`add node as first child's child</button>
+        <button>remove node id="0-5"</button>
+        <button>remove node id="0-6"</button>
+      </div>
+
+      <Chart :data="data"></Chart>
+    </form>
+  </div>
+</template>
+
+<script>
+import Chart from "./components/Chart.vue";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      data: []
+    };
+  },
+  components: {
+    Chart
+  },
+  methods: {},
+  created() {
+    fetch(
+      "https://gist.githubusercontent.com/bumbeishvili/dc0d47bc95ef359fdc75b63cd65edaf2/raw/c33a3a1ef4ba927e3e92b81600c8c6ada345c64b/orgChart.json"
+    )
+      .then(d => d.json())
+      .then(d => {
+        console.log("fetched data");
+        this.data = d;
+      });
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/org-chart-demo/src/components/Chart.vue
@@ -0,0 +1,65 @@
+<template>
+  <div  ref="svgElementContainer" >
+
+  </div>
+</template>
+
+<script>
+import * as TreeChart from "d3-org-chart";
+
+export default {
+  name: "Chart",
+  props: ["data"],
+  data() {
+    return {
+      chartReference: null
+    };
+  },
+  watch: {
+    data(value) {
+      console.log("got new data", value);
+      this.renderChart(value);
+    }
+  },
+  created() {
+    console.log(this.TreeChart);
+  },
+  methods: {
+    renderChart(data) {
+      console.log("rendering graph",this.$refs.svgElementContainer);
+      if (!this.chartReference) {
+        this.chartReference = new TreeChart();
+      }
+      this.chartReference
+        .container(this.$refs.svgElementContainer) // node or css selector
+        .data(data)
+        .svgWidth(500)
+        .initialZoom(0.4)
+        .onNodeClick(d => console.log(d + " node clicked"))
+        .render();
+
+      console.log(data);
+    }
+  }
+};
+</script>
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
